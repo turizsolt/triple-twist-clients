b811ca42c7108358511e9924b69e8dc0fff69742
turizsolt <turi.zsolt.m@gmail.com>
2018-02-17 22:34:43 +0100

    game-picked-screen now works.

--- src/app/game-picked/game-picked-screen/game-picked-screen.component.ts
+++ src/app/game-picked/game-picked-screen/game-picked-screen.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {CommunicationService} from "../../communication.service";
+import {CardInfoService} from "../../card-info.service";
+import {RoleService} from "../../role.service";
 
 @Component({
   selector: 'app-game-picked-screen',
@@ -7,20 +9,54 @@ import {CommunicationService} from "../../communication.service";
   styleUrls: ['./game-picked-screen.component.css']
 })
 export class GamePickedScreenComponent implements OnInit {
+  teams:any[];
 
   constructor(
     public communication:CommunicationService,
-    public changeDetectorRef:ChangeDetectorRef
+    public changeDetectorRef:ChangeDetectorRef,
+    private role:RoleService
   ) { }
 
   ngOnInit() {
-    this.communication.subscribe("game-picked", (data) => {
-      console.log("game-picked-screen", data);
-      this.changeDetectorRef.detectChanges();
-    });
+    this.communication.subscribe("game-picked", this.onMessage);
+    /*this.communication.emit("message", {
+      type: "game-picked",
+      event: "re-init",
+      parameters: {}
+    });*/
   }
 
   ngOnDestroy(): void {
     this.communication.unsubscribe("game-picked");
   }
+
+  private onInit(data) {
+    console.log("onInit", data);
+    this.teams = data.parameters.result.map((team) => {
+      return {
+        members: team.member,
+        cards: team.card
+          .filter(c => c.processResult == 0)
+          .map((c) => {
+          return {
+            type: c.card.type,
+            title: CardInfoService.getTitle(c.card.type),
+            symbol: CardInfoService.getSymbol(c.card.type)
+          };
+        })
+    };});
+    console.log("after onInit", this.teams);
+  }
+
+  onMessage = (data) => {
+    console.log("onMessage", data);
+    switch(data.event) {
+      case "init":
+        this.onInit(data);
+        break;
+    }
+
+    console.log("game-picked-team", data);
+    this.changeDetectorRef.detectChanges();
+  };
 }
